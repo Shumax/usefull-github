794aba7ca1353085a847d452c35184df3cf5b33d
Max Vinicius de Castro Silva <max.silva@ademicon.com.br>
2023-04-04 09:01:04 -0300

    feat: get public repositories

--- backend/src/controller/users/repos.js
+++ backend/src/controller/users/repos.js
@@ -1,4 +1,15 @@
+const octoService = require('../../services/github')
 
-module.exports = function repos(req, res) {
-  return res.send('repositor')
+module.exports = async function repos(req, res) {
+  const { username } = req.params
+
+  try {
+    
+    const data = await octoService.getPublicRepos(username)
+
+    return res.status(202).send(data)
+
+  } catch (err) {
+    return res.status(400).json({ message: err })
+  }
 }
