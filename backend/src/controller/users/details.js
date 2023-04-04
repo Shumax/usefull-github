307feb6737e8eaa07f96168dad8fad7e06534c7f
Max Vinicius de Castro Silva <max.silva@ademicon.com.br>
2023-04-04 08:49:40 -0300

    feat: get one user

--- backend/src/controller/users/details.js
+++ backend/src/controller/users/details.js
@@ -1,5 +1,16 @@
+const octoService = require('../../services/github')
 
-module.exports = function detail(req, res) {
-  return res.send('user detail')
+module.exports = async function detail(req, res) {
+  const { username } = req.params
+
+  try {
+    
+    const data = await octoService.getOneUser(username)
+
+    return res.status(202).send(data)
+
+  } catch (err) {
+    return res.status(400).json({ message: err })
+  }
 }
 
